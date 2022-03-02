ab67d9a05d82c69c6298c924404025d2341fed7f
Matt Carroll <matthewcarroll@google.com>
2022-03-02 09:30:27 -0800

    Add PDF splitter sample
    This sample is a Python CLI that uses Document AI's splitter
    processor to determine how a PDF document should be split. This
    information is then used to split the document into subdocuments
    that are saved locally as PDFs. The sample does not alter the
    input PDF.
    
    There are several flags to configure the CLI:
    
      --input/-i path to the PDF file to be split
      --output is the path to the dir to save split PDF files
               Defaults to current directory
      --project-id is the project ID used to call the Document AI API
                   Defaults to the project indicated by application
                   default credentials
      --multi-region-location indicates which region to use
                              defaults to "us"
      --split-processor-type indicates what processor type to use
                             defaults to "DOCUMENT_SPLIT_PROCESSOR"
    
    This sample will search to see if a processor of the specified
    type is already present in the specified project and location.
    If the processor already exists, it is used. If it does not exist
    a processor of the specified type will be created in the specified
    projects and location.
    
    Unit tests mock Document AI API responses and use the file
    `multi_document.pdf` to check that the PDF is split as expected.
    
    The sample depends on the Document AI Python client library to
    make Document AI AI calls and the Pike PDF library to read and
    write PDFs locally.

---
+++ pdf-splitter-python/main_test.py
@@ -0,0 +1,103 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Document AI PDF Splitter Sample Unit Tests"""
+
+import os
+import argparse
+import unittest
+import tempfile
+import shutil
+from unittest.mock import patch
+
+from google.cloud import documentai_v1beta3 as docai
+
+from main import main
+
+PROCESSOR_TYPE = 'processor-type'
+
+PROJECT_ID = 'project-id'
+LOCATION = 'location'
+PROCESSOR_ID = 'processor-id'
+PROCESSOR_NAME = f'projects/{PROJECT_ID}/locations/{LOCATION}/processors/{PROCESSOR_ID}'
+
+TEST_FILENAME = 'multi_document.pdf'
+TEST_FILE_RELATIVE_PATH = os.path.join('tests', 'resources', TEST_FILENAME)
+EXPECTED_FILENAME = 'subdoc_1_of_1_multi_document.pdf'
+
+
+class TestMain(unittest.TestCase):
+    """Test class for Document AI PDF splitter sample"""
+
+    @patch("main.DocumentProcessorServiceClient.process_document")
+    @patch("main.DocumentProcessorServiceClient.list_processors")
+    # pylint: disable=no-self-use
+    def test_split_document(self, list_processors_mock, process_document_mock):
+        """Test for Document AI PDF splitter sample"""
+
+        # Mock list processor API call to get a fake processor to use
+        list_processors_mock.return_value = docai.types.ListProcessorsResponse(
+            processors = [ docai.types.Processor(
+                type_ = PROCESSOR_TYPE,
+                name = 'projects/PROJECT_ID/locations/LOCATION/processors/PROCESSOR_ID',
+            )]
+        )
+
+        # Mock process document API call to use a fake API response
+        process_document_mock.return_value = docai.types.ProcessResponse(
+            document = docai.types.Document(
+                entities = [ docai.types.Document.Entity(
+                    page_anchor = docai.types.Document.PageAnchor(
+                        page_refs = [
+                            docai.types.Document.PageAnchor.PageRef(
+                                page = 0
+                            ),
+                            docai.types.Document.PageAnchor.PageRef(
+                                page = 1
+                            ),
+                        ]
+                    )
+                )]
+            )
+        )
+
+        # Get path to multi_document.pdf test file
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        test_filepath = os.path.join(dir_path, TEST_FILE_RELATIVE_PATH)
+
+        # Create temporary directory to add test files to
+        temp_out_dir = tempfile.mkdtemp()
+
+        # Call main function with args
+        main(argparse.Namespace(
+            input=test_filepath,
+            output_dir=temp_out_dir,
+            project_id=PROJECT_ID,
+            multi_region_location=LOCATION,
+            split_processor_type=PROCESSOR_TYPE))
+
+        expected_filepath = os.path.join(temp_out_dir, EXPECTED_FILENAME)
+
+        # Check to see that a split file was created
+        assert os.path.isfile(expected_filepath)
+
+        # Check to see that the file is not empty
+        filesize = os.path.getsize(expected_filepath)
+        assert filesize > 0
+
+        # Clean up test files
+        shutil.rmtree(temp_out_dir)
+
+if __name__ == '__main__':
+    unittest.main()
