be867a6db50aa7538ebe19bfccec79ab26f456c5
Karl Weinmeister <kweinmeister@google.com>
2022-04-01 11:00:21 -0500

    ci: Add OwlBot configuration

---
+++ owlbot.py
@@ -0,0 +1,35 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import synthtool as s
+import synthtool.gcp as gcp
+from synthtool.languages import python
+
+templated_files = gcp.CommonTemplates().py_library()
+
+# Copy the standard noxfile from templated_files
+s.move(templated_files / "noxfile.py")
+
+# Update BLACK_PATHS in order to run black on all files
+s.replace(
+    "noxfile.py",
+    """BLACK_PATHS = \["docs", "google", "tests", "noxfile.py", "setup.py"\]""",
+    """BLACK_PATHS = ["."]""",
+)
+
+# ----------------------------------------------------------------------------
+# Run blacken session
+# ----------------------------------------------------------------------------
+
+s.shell.run(["nox", "-s", "blacken"], hide_output=False)
