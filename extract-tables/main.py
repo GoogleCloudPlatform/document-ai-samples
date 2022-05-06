25e28016e01689cd5551c3c0f6ba5ba46e4716fd
Holt Skinner <holtskinner@google.com>
2022-05-06 15:04:24 -0500

    Added Text Anchor Extraction and output to CSV

--- extract-tables/main.py
+++ extract-tables/main.py
@@ -49,6 +49,38 @@ def online_process(
     return result.document
 
 
+def get_table_data(
+    rows: Sequence[documentai.Document.Page.Table.TableRow], text: str
+) -> List[List[str]]:
+    """
+    Get Text data from table rows
+    """
+    all_values: List[List[str]] = []
+    for row in rows:
+        current_row_values: List[str] = []
+        for cell in row.cells:
+            current_row_values.append(
+                text_anchor_to_text(cell.layout.text_anchor, text)
+            )
+        all_values.append(current_row_values)
+    return all_values
+
+
+def text_anchor_to_text(text_anchor: documentai.Document.TextAnchor, text: str) -> str:
+    """
+    Document AI identifies table data by their offsets in the entirity of the
+    document's text. This function converts offsets to a string.
+    """
+    response = ""
+    # If a text segment spans several lines, it will
+    # be stored in different text segments.
+    for segment in text_anchor.text_segments:
+        start_index = int(segment.start_index)
+        end_index = int(segment.end_index)
+        response += text[start_index:end_index]
+    return response.strip().replace("\n", " ")
+
+
 PROJECT_ID = "YOUR_PROJECT_ID"
 LOCATION = "YOUR_PROJECT_LOCATION"  # Format is 'us' or 'eu'
 PROCESSOR_ID = "FORM_PARSER_ID"  # Create processor in Cloud Console
@@ -67,32 +99,20 @@ document = online_process(
     mime_type=MIME_TYPE,
 )
 
-
-def get_table_data(rows: Sequence[documentai.TableRow]) -> List[List[str]]:
-    """
-    Get Text data from table rows
-    """
-    all_values: List[List[str]] = []
-    for row in rows:
-        current_row_values: List[str] = []
-        for cell in row.cells:
-            current_row_values.append(cell.layout.text_anchor.content)
-        all_values.append(current_row_values)
-    return all_values
-
-
 header_row_values: List[List[str]] = []
 body_row_values: List[List[str]] = []
 
 for page in document.pages:
     for table in page.tables:
-        header_row_values = get_table_data(table.header_rows)
-        body_row_values = get_table_data(table.body_rows)
-
+        header_row_values = get_table_data(table.header_rows, document.text)
+        body_row_values = get_table_data(table.body_rows, document.text)
 
 # Create a Pandas Dataframe to print the values in tabular format.
 df = pd.DataFrame(
-    data=body_row_values, columns=pd.MultiIndex.from_arrays(header_row_values)
+    data=body_row_values,
+    columns=pd.MultiIndex.from_arrays(header_row_values),
 )
 
 print(df)
+
+df.to_csv("table_data.csv", index=False)
