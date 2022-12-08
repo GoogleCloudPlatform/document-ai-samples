fa26046d53c754484cb95ff51da58375d87ad0d9
Karl Weinmeister <kweinmeister@google.com>
2022-12-07 20:59:42 -0600

    test: revert to correct test

--- web-app-demo/Frontend/src/app/app.component.spec.ts
+++ web-app-demo/Frontend/src/app/app.component.spec.ts
@@ -67,7 +67,7 @@ describe("AppComponent", () => {
   it("should create the app", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app).toBeFalsy();
+    expect(app).toBeTruthy();
   });
 
   it(`should have as title 'Document AI Modular App'`, () => {
