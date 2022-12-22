81efb77ce8428468baea0965ecb6369aa98e7187
Debora Elkin <deboraelkin@google.com>
2022-12-22 15:03:25 +1100

    Update creeate_doc_reference_table.sh

--- bq-connector/setup/creeate_doc_reference_table.sh
+++ bq-connector/setup/creeate_doc_reference_table.sh
@@ -1,5 +1,5 @@
 bq --project_id=$PROJECT_ID \
-   --dataset_id=$SUBMISSION_HUB_DATASET_ID \
+   --dataset_id=$DATASET_ID \
    mk --table \
     --description="Used by docai_bq_connector to keep track of documents processed and to match up results of HITL reviews back to the original document" \
     --schema="./setup/doc_reference_table_schema" \
