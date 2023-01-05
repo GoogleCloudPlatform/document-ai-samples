b7ee6c70ffa3f01d3f827e9532f2fb5a6fd7ad7d
Debora Elkin <deboraelkin@google.com>
2023-01-05 16:07:15 +1100

    Fixed name of doc_reference table in creation script

--- bq-connector/setup/creeate_doc_reference_table.sh
+++ bq-connector/setup/creeate_doc_reference_table.sh
@@ -3,5 +3,5 @@ bq --project_id=$PROJECT_ID \
    mk --table \
     --description="Used by docai_bq_connector to keep track of documents processed and to match up results of HITL reviews back to the original document" \
     --schema="./setup/doc_reference_table_schema" \
-    doc_reference2
+    doc_reference
     
