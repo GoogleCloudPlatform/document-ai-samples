0ee566d016c1d7660db9312f14f478a25d6791a9
Mark Servidio <mservidio@google.com>
2023-01-05 11:13:11 -0500

    updates

--- bq-connector/setup/create_doc_reference_table.sh
+++ bq-connector/setup/create_doc_reference_table.sh
@@ -3,6 +3,6 @@ bq --project_id=$PROJECT_ID \
    --dataset_id=$DATASET_ID \
    mk --table \
     --description="Used by docai_bq_connector to keep track of documents processed and to match up results of HITL reviews back to the original document" \
-    --schema="./setup/doc_reference_table_schema" \
+    --schema="./setup/doc_reference_table_schema.json" \
     doc_reference
     
