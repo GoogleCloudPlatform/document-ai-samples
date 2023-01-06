0224d64f683cdc65a7c15a8fac73db6f8d7babfb
Mark Servidio <mservidio@google.com>
2023-01-05 21:01:53 -0500

    Update ProcessedDocument.py

--- bq-connector/docai_bq_connector/doc_ai_processing/ProcessedDocument.py
+++ bq-connector/docai_bq_connector/doc_ai_processing/ProcessedDocument.py
@@ -23,7 +23,9 @@ from google.cloud.documentai_v1 import Document
 
 
 class ProcessedDocument:
-    def __init__(self, document: Document, dictionary, hitl_operation_id: Optional[str] = None):
+    def __init__(
+        self, document: Document, dictionary, hitl_operation_id: Optional[str] = None
+    ):
         self.document = document
         self.dictionary = dictionary
         self.hitl_operation_id = hitl_operation_id
