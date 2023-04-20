a2883c9eb2446f9565b233c05a135f45f57a2fa5
Holt Skinner <holtskinner@google.com>
2023-04-20 16:29:01 -0500

    Add Generative AI App Builder Document Search Demo

---
+++ document-search/main.py
@@ -0,0 +1,57 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Flask Web Server"""
+
+import os
+
+from flask import Flask
+from flask import render_template
+
+from google.api_core.exceptions import ResourceExhausted
+from werkzeug.exceptions import HTTPException
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def index() -> str:
+    """
+    Web Server, Homepage
+    """
+
+    return render_template("index.html")
+
+@app.errorhandler(Exception)
+def handle_exception(ex: Exception):
+    """
+    Handle Application Exceptions
+    """
+    message_error = "An Unknown Error Occured"
+
+    # Pass through HTTP errors
+    if isinstance(ex, HTTPException):
+        message_error = ex.get_description()
+    elif isinstance(ex, ResourceExhausted):
+        message_error = ex.message
+    else:
+        message_error = str(ex)
+
+    return render_template(
+        "index.html",
+        message_error=message_error,
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
